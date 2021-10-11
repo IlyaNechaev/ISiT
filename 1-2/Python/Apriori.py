f7ca13295d1a3114f49dc1a819086982868c77e0
Ilya Nechaev <inechaev@croc.ru>
2021-10-11 22:34:59 +0300

    Попытка использовать dataset как DataFrame

--- 1-2/Python/Apriori.py
+++
@@ -1,8 +0,0 @@
-from apriori_python import apriori
-
-transactions= [['Тетрадь', 'Ручка', 'Краски'],
-               ['Тетрадь', 'Ручка', 'кисть'],
-               ['Краски', 'Ручка', 'Ластик']]
-
-freqItemSet, rules = apriori(transactions, minSup=0.5, minConf=0.5)
-print(rules)
